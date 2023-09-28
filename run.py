95c5fddb6e4431db573e629cd4dee098031245e1
Mehran <mr.mehran75@gmail.com>
2023-09-28 12:52:27 +0330

    resize photos for consistency

--- run.py
+++ run.py
@@ -13,16 +13,30 @@ class Cascade_model(Enum):
         Name = "face"
         Model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         Scale_factor = 1.3
-        Min_Neighbors = 4
+        Min_Neighbors = 5
     class Smile(Enum):
         Name = "smile"
         Model = cv2.CascadeClassifier('haarcascade_smile.xml')
-        Scale_factor = 1.1
-        Min_Neighbors = 3
+        Scale_factor = 1.3
+        Min_Neighbors = 20
+
+# get best size based on aspect ratio
+def get_size_ratio(min_width, min_height, ratio = (4,3)):
+    if ratio[0] > ratio[1]:
+        if min_width > min_height:
+            min_height = min_width * ratio[1] / ratio[0]
+        else:
+            min_width = min_height * ratio[0] / ratio[1]
+    else:
+        if min_width > min_height:
+            min_height = min_width * ratio[1] / ratio[0]
+        else:
+            min_width = min_height * ratio[0] / ratio[1]
+    return round(min_width), round(min_height)
 
 # crop parts from images and save in another folder
-def part_croper(source_folder_path, target_folder_path, cascade_model):
-    images = os.listdir(source_folder_path)
+def part_croper(input_folder_path, target_folder_path, cascade_model, should_resize = False, height = 128, width = 128):
+    images = os.listdir(input_folder_path)
 
     # remove old files
     files_to_remove = os.listdir(target_folder_path)
@@ -32,7 +46,7 @@ def part_croper(source_folder_path, target_folder_path, cascade_model):
 
     if len(images) > 0:
         for image in images:
-            img = cv2.imread(os.path.join(source_folder_path,image))
+            img = cv2.imread(os.path.join(input_folder_path,image))
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # detects parts in the input image
@@ -47,19 +61,25 @@ def part_croper(source_folder_path, target_folder_path, cascade_model):
             # save detected parts
             if len(parts) > 0:
                 for i, (x, y, w, h) in enumerate(parts):
+
+                    # get the best width and height for being ready to resize
+                    if should_resize:
+                        w, h = get_size_ratio(w,h, (width, height))
                     part = img[y:y + h, x:x + w]
                     file_name = os.path.join(target_folder_path, f'{image[:-4]}-{cascade_model.Name.value}{i+1}.jpg')
                     if os.path.exists(file_name):
                         os.remove(file_name)
+
+                    # resize the photo for consistency
+                    if should_resize:
+                        part = cv2.resize(part, (width, height), interpolation=cv2.INTER_LINEAR)
                     cv2.imwrite(file_name, part)
                     print(file_name)
             else :
                 print(image,f"No {cascade_model.Name.value} detected!")
 
+# detect faces
+part_croper(source_folder_path, faces_folder_path, Cascade_model.Face.value, True , 256, 256)
 
-# crop faces from each image
-part_croper(source_folder_path,faces_folder_path,Cascade_model.Face.value)
-
-# crop smiles from each face
-part_croper(faces_folder_path,smiles_folder_path,Cascade_model.Smile.value)
-
+# detect smiles
+part_croper(faces_folder_path, smiles_folder_path, Cascade_model.Smile.value, 128, 64)
